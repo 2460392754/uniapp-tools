7c0beb65b5b10f6f4432dc6ae7aaf860c026f188
pocky <39649938+2460392754@users.noreply.github.com>
2019-06-17 11:41:24 +0800

    Update and rename 1 to demo.vue

---
+++ lazyLoad/demo.vue
@@ -0,0 +1,49 @@
+<template>
+    <view>
+        <scroll-view id="scroll"
+                     :scroll-y="true"
+                     @scroll="scroll">
+            <v-lazyLoad src="/static/img1.jpg"
+                        mode="widthFix"></v-lazyLoad>
+
+            <v-lazyLoad src="/static/img2.jpg"
+                        mode="widthFix"></v-lazyLoad>
+
+            <v-lazyLoad src="/static/img3.jpg"
+                        mode="widthFix"></v-lazyLoad>
+
+            <v-lazyLoad src="/static/img4.jpg"
+                        mode="widthFix"></v-lazyLoad>
+
+            <v-lazyLoad src="/static/img5.jpg"
+                        mode="widthFix"></v-lazyLoad>
+        </scroll-view>
+    </view>
+</template>
+
+<script>
+import lazyLoad from "../../utils/lazyLoad"
+import VLazyLoad from "../../components/lazyLoad"
+
+export default {
+    methods: {
+        scroll (e) {
+            lazyLoad.scroll(); // 监听滚动条事件
+        }
+    },
+
+    mounted () {
+        lazyLoad.init("#scroll"); // 填写scroll-view标签id，初始化插件
+    },
+
+    components: {
+        VLazyLoad
+    }
+}
+</script>
+
+<style>
+#scroll {
+    height: 700px; // 需要给scroll-view设置高度
+}
+</style>
